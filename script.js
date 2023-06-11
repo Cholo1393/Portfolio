839869c3c734b60ecc3715612b429865c86c2835
Cholo1393 <jean-christophe.matti@epitech.eu>
2023-06-12 00:21:55 +0200

    portfolio

--- script.js
+++ script.js
@@ -138,3 +138,7 @@ window.addEventListener('load', function() {
 //     }, 300);
 //   });
 // });
+// Après avoir traité la soumission réussie du formulaire
+
+// Redirigez l'utilisateur vers la page de confirmation personnalisée
+window.location.href = 'confirmation.html';
